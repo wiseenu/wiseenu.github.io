316efa8908cfd1098998e385f4efc93136ff7b1c
wiseenu <wisnupw6@gmail.com>
2022-03-07 12:21:22 +0700

    Tugas

---
+++ precache-manifest.1bad4e952d7240b0d38c78778306faa9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b858592978d4d98373",
+    "url": "/css/app.45369266.css"
+  },
+  {
+    "revision": "2e943f24c17feea0019aef79c6b9d176",
+    "url": "/img/Project1.2e943f24.jpg"
+  },
+  {
+    "revision": "747f266596f78b34322a4eb1e9f3eca6",
+    "url": "/img/android.747f2665.jpg"
+  },
+  {
+    "revision": "064ecb9ff1eba788a203f8f75c409e26",
+    "url": "/img/contact.064ecb9f.jpg"
+  },
+  {
+    "revision": "f3b1a2341996b9512bb7f75b6b104770",
+    "url": "/img/email.f3b1a234.png"
+  },
+  {
+    "revision": "5b0f9e8b9733e17120cfcde7a431d70e",
+    "url": "/img/emblem.5b0f9e8b.jpg"
+  },
+  {
+    "revision": "4f031d7dc80084d7a61fac4ce5d9c846",
+    "url": "/img/ig2.4f031d7d.png"
+  },
+  {
+    "revision": "32bd59512d5b5663c10fa74071a45688",
+    "url": "/img/in.32bd5951.png"
+  },
+  {
+    "revision": "133a03f2a3a2b06d2017d839f80814fa",
+    "url": "/img/soon.133a03f2.jpeg"
+  },
+  {
+    "revision": "997dae06c3d4e268e8777e572b3e5f3c",
+    "url": "/img/wisnu.997dae06.jpg"
+  },
+  {
+    "revision": "afe37e412265c1e2a4ec8a3f916721b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b858592978d4d98373",
+    "url": "/js/app.14b38182.js"
+  },
+  {
+    "revision": "c8cf5647a8a85df31168",
+    "url": "/js/chunk-vendors.43180a49.js"
+  },
+  {
+    "revision": "e97c5160991cd3ef67cb4ebb1761dde9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
